ca91dc4b1e8071c916c6fdccc01b65e2e473fc7e
Mick <Mick@mydom.de>
2014-07-05 15:18:18 +0200

    Correct usage of Singleton-pattern

--- src/filecollector/controller/MainController.java
+++ src/filecollector/controller/MainController.java
@@ -5,8 +5,6 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
 
-import filecollector.controller.collectorWorker.DirectoryWorker;
-import filecollector.controller.collectorWorker.DirectoryWorkerRunnable;
 import filecollector.controller.ExecutorSingleton;
 import filecollector.controller.ExecutorSingleton.WhichExecutor;
 import filecollector.controller.collectorWorker.WorkerCounter;
@@ -24,6 +22,8 @@ public class MainController {
 	public void entryApplikation (String[] args) {
 
 		collector = new Collector (Paths.get (args[0]));
+		new ExecutorSingleton (WhichExecutor.CACHEDPOOL);
+
 		// startFirstWorkerThread ();
 		// callExecutor ();
 
@@ -51,6 +51,7 @@ public class MainController {
 		log.error ("FutureGet DIFF(ms) : " + futureDiffTime);
 		collector.printTest ();
 
+		ExecutorSingleton.getInstance ().shutdownExecutor ();
 	}
 
 	private void testCallExecutor () {
@@ -71,14 +72,16 @@ public class MainController {
 	}
 
 	private void callExecutor_FutureGet () {
-		ExecutorSingleton executor = new ExecutorSingleton (WhichExecutor.CACHEDPOOL);
-		executor.executeWorker (collector.getCollectionView (CollectionViewSelectorEnum.TEST_FUTURE_GET));
-		executor.shutdownExecutor ();
+//		ExecutorSingleton executor = new ExecutorSingleton (WhichExecutor.CACHEDPOOL);
+//		executor.executeWorker (collector.getCollectionView (CollectionViewSelectorEnum.TEST_FUTURE_GET));
+//		executor.shutdownExecutor ();
+		ExecutorSingleton.getInstance ().executeWorker (collector.getCollectionView (CollectionViewSelectorEnum.TEST_FUTURE_GET));
 	}
 
 	private void callExecutor_Sleep () {
-		ExecutorSingleton executor = new ExecutorSingleton (WhichExecutor.CACHEDPOOL);
-		executor.executeWorker (collector.getCollectionView (CollectionViewSelectorEnum.TEST_SLEEP));
+//		ExecutorSingleton executor = new ExecutorSingleton (WhichExecutor.CACHEDPOOL);
+//		executor.executeWorker (collector.getCollectionView (CollectionViewSelectorEnum.TEST_SLEEP));
+		ExecutorSingleton.getInstance ().executeWorker (collector.getCollectionView (CollectionViewSelectorEnum.TEST_SLEEP));
 		/**
 		 * !!! Das ist nicht immer SICHER, allWorkerFinish kann 0 sein obwohl
 		 * noch weitere Verzeichnissse da sind. Wird nur ueber sleep 10
@@ -89,6 +92,6 @@ public class MainController {
 			SleepUtils.safeSleep (TimeUnit.MILLISECONDS, 10);
 			log.info ("ALIVE");
 		} while (!WorkerCounter.allWorkerFinish ());
-		executor.shutdownExecutor ();
+//		executor.shutdownExecutor ();
 	}
 }
