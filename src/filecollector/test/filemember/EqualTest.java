19bae2ab7ce3e35401229c9abea7b2f9ae652d07
Mick <Mick@mydom.de>
2014-08-11 07:32:45 +0200

    Bug fix + JUnit: StackOverflowException at FileSystemMember hashCode() recursive call of outer class hashCode(). getOuterType() only generated by Eclipse(->Source), because its a hidden attribute and not useful!; Test-Suite for equals(), compareTo(), hashCode() impl

--- src/filecollector/test/filemember/EqualTest.java
+++ src/filecollector/test/filemember/EqualTest.java
@@ -10,7 +10,6 @@ import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 
 import filecollector.model.filemember.DirectoryMember;
-import filecollector.model.filemember.FileSystemMember.FileTimes;
 
 // Dringend Tear, before after nutzen
 public class EqualTest {
@@ -21,31 +20,45 @@ public class EqualTest {
 	DirectoryMember d1 = new DirectoryMember(p1);
 	DirectoryMember d2 = new DirectoryMember(p2);
 	DirectoryMember d3 = new DirectoryMember(p3);
+//	DirectoryMember d3 = d1;
 	FileTime ft1 = FileTime.from(5, TimeUnit.DAYS);
 	FileTime ft2 = FileTime.from(2, TimeUnit.DAYS);
+	FileTime ft3 = FileTime.from(5, TimeUnit.DAYS);
 	
-//	@Test
-//	public void test() {
-//		fail("Not yet implemented");
-//	}
 	@Test
-	public void sameContent() {
+	public void objectsNotSame() {
+		assertFalse(d1 == d2);
+		assertFalse(d1 == d3);
+		assertFalse(d2 == d3);
+		assertFalse(ft1 == ft2);
+		assertFalse(ft1 == ft3);
+		assertFalse(ft2 == ft3);
+	}
+	@Test
+	public void equalsContent() {
+		// same
 		d1.setFileTimes(d1.new FileTimes(ft1, ft1, ft1));
 		d2.setFileTimes(d2.new FileTimes(ft1, ft1, ft1));
 		assertTrue(d1.equals(d2));
+		// diff path
+		d3.setFileTimes(d3.new FileTimes(ft1, ft1, ft1));
+		assertFalse(d1.equals(d3));
+		// diff time
+		d2.setFileTimes(d2.new FileTimes(ft1, ft1, ft2));
+		assertFalse(d1.equals(d2));
 	}
-	@Test
-	public void notSameContent() {
+//	@Test
+	public void notSameContentEquals() {
 		// diff path
 		d1.setFileTimes(d1.new FileTimes(ft1, ft1, ft1));
 		d3.setFileTimes(d3.new FileTimes(ft1, ft1, ft1));
 		assertFalse(d1.equals(d3));
 		// diff time
-		d2.setFileTimes(d1.new FileTimes(ft1, ft1, ft2));
+		d2.setFileTimes(d2.new FileTimes(ft1, ft1, ft2));
 		assertFalse(d1.equals(d2));
 	}
 	@Test
-	public void compare() {
+	public void compareContent() {
 		// gleich
 		d1.setFileTimes(d1.new FileTimes(ft1, ft1, ft1));
 		d2.setFileTimes(d2.new FileTimes(ft1, ft1, ft1));
@@ -56,5 +69,23 @@ public class EqualTest {
 		// path test > other
 		d3.setFileTimes(d3.new FileTimes(ft1, ft1, ft1));
 		assertEquals(1, d1.compareTo(d2));
+		// only lastModifiedTime will compared
+		d2.setFileTimes(d2.new FileTimes(ft2, ft2, ft1));
+		assertEquals(0, d1.compareTo(d2));
+	}
+	@Test
+	public void hashCodeTests() {
+		// FileTimes = null
+		assertTrue(d1.hashCode() == d2.hashCode());
+		// same content
+		d1.setFileTimes(d1.new FileTimes(ft1, ft1, ft1));
+		d2.setFileTimes(d2.new FileTimes(ft1, ft1, ft1));
+		assertTrue(d1.hashCode() == d2.hashCode());
+		// diff Path
+		d3.setFileTimes(d3.new FileTimes(ft1, ft1, ft1));
+		assertFalse(d1.hashCode() == d3.hashCode());
+		// diff Times
+		d2.setFileTimes(d2.new FileTimes(ft1, ft2, ft1));
+		assertFalse(d1.hashCode() == d2.hashCode());
 	}
 }
